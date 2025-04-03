97e6cdd9077e5a147231d8d25ce4e9f48ef54897
prabhakarank7 <62760472+prabhakarank7@users.noreply.github.com>
2025-04-03 21:50:07 +0530

    Update DataWarehouse_db_schema_Init.sql

--- scripts/DataWarehouse_db_schema_Init.sql
+++ scripts/DataWarehouse_db_schema_Init.sql
@@ -1,3 +1,22 @@
+/*
+=====================================================================
+CREATE DATABASE AND SCHEMAS
+=====================================================================
+
+Script purpose:
+
+	This script creates a new database 'DataWarehouse' after checking if the database already exists. If the database 
+	already exists, it is dropped and recreated. After the database creation, three schemas, bronze, silver and gold 
+	are created within the database.
+
+Warning:
+	
+	Please be warned that running this script will drop the entire 'DataWarehouse' database permanently if it exists and 
+	along with that all the data in the database will be permanently deleted and cannot be recovered.
+
+	Proceed with caution and ensure you have a proper backup(s) before running this script.
+*/
+
 USE master;
 GO
 
