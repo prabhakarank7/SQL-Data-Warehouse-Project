bd5bd1da3309c0d4359b4b4852ed8567f653b19d
prabhakarank7 <62760472+prabhakarank7@users.noreply.github.com>
2025-04-03 21:33:18 +0530

    Update DataWarehouse_db_schema_Init.sql

--- scripts/DataWarehouse_db_schema_Init.sql
+++ scripts/DataWarehouse_db_schema_Init.sql
@@ -1,1 +1,22 @@
+USE master;
+GO
 
+--Check if any existing database with same name and drop it
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+	END;
+	GO
+
+-- Create 'DataWarehouse' database & USE
+CREATE DATABASE DataWarehouse;
+GO
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
