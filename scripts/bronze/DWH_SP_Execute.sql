51de65eb1ac45bc896a15c5b19e97222fdf08679
prabhakarank7 <62760472+prabhakarank7@users.noreply.github.com>
2025-04-06 23:54:42 +0530

    Update DWH_SP_Execute.sql

--- scripts/bronze/DWH_SP_Execute.sql
+++ scripts/bronze/DWH_SP_Execute.sql
@@ -1,1 +1,8 @@
+USE DataWarehouse;
 
+/*
+Script Purpose: The purpose of this script is to execute the stored procedure which loads the data into the tables
+in the bronze schema.
+*/
+
+EXEC bronze.load_bronze;
